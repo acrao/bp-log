3ad7d0280f8e38d8989d97465fb7526d5ece4bf2
Aditya Rao <adityacrao@gmail.com>
2012-04-14 22:19:00 -0700

    Additions to Measurement model
     *Added method to return dict representation of model

--- bp_log/models.py
+++ bp_log/models.py
@@ -1,12 +1,19 @@
-import datetime
 from google.appengine.ext import db
 
-def Measurements(db.Model):
+class Measurement(db.Model):
     sys = db.IntegerProperty()
     dia = db.IntegerProperty()
     pulse = db.IntegerProperty()
     recorded_time = db.DateTimeProperty(auto_now_add=True)
-    user = db.UserProperty()
+    user = db.UserProperty(auto_current_user_add=True)
 
     def get_mat(self):
-        return (sys - dia) * 3
+        return (self.sys - self.dia) * 3
+
+    def to_dict(self):
+        return {"sys" : self.sys,
+                "dia" : self.dia,
+                "pulse" : self.pulse,
+                "time" : self.recorded_time.strftime("%H:%M:%S %m/%d/%Y"),
+                "user" : self.user.email(),
+                "mat" : self.get_mat()}
